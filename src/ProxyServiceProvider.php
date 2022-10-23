8fef33058386320206feec2d6a4a0b8778e76ad9
Saed Yousef <saed.alzaben@gmail.com>
2022-10-23 21:20:41 +0200

    Remove routes, and unneeded service providers.

--- src/ProxyServiceProvider.php
+++ src/ProxyServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace Yousefpackage\JoeProxy;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\ServiceProvider;
-use Yousefpackage\JoeProxy\Repositories\Contracts\AlertRepository;
-use Yousefpackage\JoeProxy\Repositories\Contracts\LogRepository;
-use Yousefpackage\JoeProxy\Repositories\DatabaseAlertRepository;
+use Illuminate\Database\ConnectionResolverInterface;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Yousefpackage\JoeProxy\Repositories\DatabaseLogRepository;
+use Yousefpackage\JoeProxy\Repositories\DatabaseAlertRepository;
+use Yousefpackage\JoeProxy\Repositories\Contracts\LogRepository;
+use Yousefpackage\JoeProxy\Repositories\Contracts\AlertRepository;
 
 class ProxyServiceProvider extends ServiceProvider
 {
@@ -27,11 +30,36 @@ class ProxyServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
+    /**
+     * @throws BindingResolutionException
+     */
     public function register()
     {
         // Binding contracts to repositories.
         foreach ($this->repositories as $abstract => $concrete) {
             $this->app->bind($abstract, $concrete);
         }
+
+        $query = $this->queryBuilder();
+        foreach ($this->repositories as $repository) {
+            $this->app->afterResolving($repository, function ($instance) use($query) {
+                $instance->setQuery($query);
+            });
+        }
+    }
+
+    /**
+     * @throws BindingResolutionException
+     */
+    private function queryBuilder(): Builder
+    {
+        /** @var ConnectionResolverInterface $connectionInterface */
+        $connectionInterface = $this->app->make(ConnectionResolverInterface::class);
+
+        return $connectionInterface
+            ->connection()
+            ->table('proxy_key_value_storage')
+            ->newQuery()
+            ->where('key');
     }
 }
--- src/Repositories/DatabaseLogRepository.php
+++ src/Repositories/DatabaseLogRepository.php
@@ -2,9 +2,20 @@
 
 namespace Yousefpackage\JoeProxy\Repositories;
 
+use Illuminate\Database\Query\Builder;
 use Yousefpackage\JoeProxy\Repositories\Contracts\LogRepository;
 
 class DatabaseLogRepository implements LogRepository
 {
+    private Builder $query;
 
+
+    /**
+     * @param Builder $query
+     * @return void
+     */
+    public function setQuery(Builder $query)
+    {
+        $this->query = $query;
+    }
 }
